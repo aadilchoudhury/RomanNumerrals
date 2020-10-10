a197bcac821d5b5b1a3b836b6b67fe1b9824b735
Aadil Choudhury <aadil@thinkbinary.co.uk>
2020-10-10 18:27:35 +0100

    Able to basic numbers with subtraction

--- src/roman.py
+++ src/roman.py
@@ -1,7 +1,32 @@
 def int_to_roman(input_number):
-    if input_number == 1:
-        return "I"
-    if input_number > 2:
-        return "IV"
+    result = ""
 
-    return "V"
+    while input_number >= 1000:
+        result = result + "M"
+        input_number = input_number - 1000
+
+    while input_number >= 500:
+        result = result + "D"
+        input_number = input_number - 500
+
+    while input_number >= 100:
+        result = result + "C"
+        input_number = input_number - 100
+
+    while input_number >= 50:
+        result = result + "L"
+        input_number = input_number - 50
+
+    while input_number >= 10:
+        result = result + "X"
+        input_number = input_number - 10
+
+    while input_number >= 5:
+        result = result + "V"
+        input_number = input_number - 5
+
+    while input_number >= 1:
+        result = result + "I"
+        input_number = input_number - 1
+
+    return result
--- tests/romanNumeralTests.py
+++ tests/romanNumeralTests.py
@@ -19,6 +19,27 @@ class RomanNumeralTests(unittest.TestCase):
     def test_five(self):
         self.assertEqual("V", int_to_roman(5))
 
+    def test_six(self):
+        self.assertEqual("VI", int_to_roman(6))
+
+    def test_seven(self):
+        self.assertEqual("VII", int_to_roman(7))
+
+    def test_eight(self):
+        self.assertEqual("VIII", int_to_roman(8))
+
+    def test_nine(self):
+        self.assertEqual("IX", int_to_roman(9))
+
+    def test_ten(self):
+        self.assertEqual("X", int_to_roman(10))
+
+    def test_eleven(self):
+        self.assertEqual("XI", int_to_roman(11))
+
+    def test_twelve(self):
+        self.assertEqual("XII", int_to_roman(12))
+
 
 if __name__ == '__main__':
     unittest.main()
